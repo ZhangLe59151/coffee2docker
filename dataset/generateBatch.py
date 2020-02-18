d770f869ed518cb642b673a173b40592840b4595
ZhangLe59151 <56253634+ZhangLe59151@users.noreply.github.com>
2020-02-18 19:16:24 +0800

    update batch generate

--- dataset/generateBatch.py
+++ dataset/generateBatch.py
@@ -4,6 +4,7 @@
 import tensorflow as tf 
 from tfrecordCreate import image_feature_description
 
+tf.compat.v1.enable_eager_execution()
 filename = 'images.tfrecords'
 # dataset = tf.data.TFRecordDataset(filenames = [filename])
 
@@ -11,20 +12,20 @@ num_workers = 1
 worker_index = 0
 num_epochs = 1
 shuffle_buffer_size = 1
-num_map_threads = 1
+num_map_threads = 2
 
 def parser_fn(example_photo):
-  features = {
-    'height': tf.io.FixedLenFeature([], tf.int64),
-    'width': tf.io.FixedLenFeature([], tf.int64),
-    'depth': tf.io.FixedLenFeature([], tf.int64),
-    'label': tf.io.FixedLenFeature([], tf.int64),
-    'image_raw': tf.io.FixedLenFeature([], tf.string),
-  }
-  parsed_features = tf.parse_single_example(example_photo, features=features)
-  labels = parsed_features['label']
-  images = parsed_features['image_raw']
-  return images, labels
+  parsed_features = tf.io.parse_single_example(example_photo, image_feature_description)
+  # labels = parsed_features['label']
+  # images = parsed_features['image_raw']
+  images = tf.image.decode_jpeg(parsed_features['image_raw'])
+  height = tf.cast(parsed_features['height'], tf.int64)
+  height = 4032
+  width = tf.cast(parsed_features['width'], tf.int64)
+  width = 3024
+  image = tf.reshape(images, [height, width, 3])
+  label = tf.cast(parsed_features['label'], tf.int64)
+  return image, label
 
 d = tf.data.TFRecordDataset(filename)
 d = d.shard(num_workers, worker_index)
@@ -32,5 +33,13 @@ d = d.repeat(num_epochs)
 d = d.shuffle(shuffle_buffer_size)
 dataset = d.map(parser_fn, num_parallel_calls=num_map_threads)
 
-for imgs, labels in dataset:
-  print(imgs)
+iterator = dataset.make_one_shot_iterator()
+images, labels = iterator.get_next()
+print(labels.numpy())
+
+for item, label in dataset:
+  # print(item.numpy())
+  # print(label.numpy())
+  pass
+
+  
--- dataset/tfrecordRead.py
+++ dataset/tfrecordRead.py
@@ -13,20 +13,6 @@ cwd = os.getcwd()
 root = cwd
 raw_image_dataset = tf.data.TFRecordDataset('images.tfrecords')
 
-def _bytes_feature(value):
-  """Returns a bytes_list from a string / byte."""
-  if isinstance(value, type(tf.constant(0))):
-    value = value.numpy() # BytesList won't unpack a string from an EagerTensor.
-  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
-
-def _float_feature(value):
-  """Returns a float_list from a float / double."""
-  return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
-
-def _int64_feature(value):
-  """Returns an int64_list from a bool / enum / int / uint."""
-  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
-
 image_feature_description = {
   'height': tf.io.FixedLenFeature([], tf.int64),
   'width': tf.io.FixedLenFeature([], tf.int64),
@@ -34,17 +20,6 @@ image_feature_description = {
   'label': tf.io.FixedLenFeature([], tf.int64),
   'image_raw': tf.io.FixedLenFeature([], tf.string),
 }
-
-def image_example(image_string, label):
-  image_shape = tf.image.decode_jpge(image_string).shape
-  feature = {
-    'height': _int64_feature(image_shape[0]),
-    'width': _int64_feature(image_shape[1]),
-    'depth': _int64_feature(image_shape[2]),
-    'label': _int64_feature(label),
-    'image_raw': _bytes_feature(image_string),
-  }
-  return tf.train.Example(features= tf.train.Features(feature = feature))
 
 def _parse_image_function(example_proto):
   return tf.io.parse_single_example(example_proto, image_feature_description)
