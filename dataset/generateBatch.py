64213fbcb5e5722016416fa40db6de4371433f15
ZhangLe59151 <56253634+ZhangLe59151@users.noreply.github.com>
2020-02-18 20:01:26 +0800

    udpate

--- dataset/generateBatch.py
+++ dataset/generateBatch.py
@@ -14,15 +14,25 @@ num_epochs = 1
 shuffle_buffer_size = 1
 num_map_threads = 2
 
+def getShape():
+  parsed_features = tf.io.parse_single_example(example_photo, image_feature_description)
+  height = parsed_features['height']
+  width = tf.cast(parsed_features['width'], tf.int64)
+  print(height, width)
+  
 def parser_fn(example_photo):
   parsed_features = tf.io.parse_single_example(example_photo, image_feature_description)
   # labels = parsed_features['label']
   # images = parsed_features['image_raw']
   images = tf.image.decode_jpeg(parsed_features['image_raw'])
-  height = tf.cast(parsed_features['height'], tf.int64)
-  height = 4032
-  width = tf.cast(parsed_features['width'], tf.int64)
-  width = 3024
+  # height = parsed_features['height']
+  # height = 4032
+  # width = tf.cast(parsed_features['width'], tf.int64)
+  # width = 3024
+  height, width = getShape()
+  height = height[0].numpy()
+  width = width[0].numpy()
+  print(height, width)
   image = tf.reshape(images, [height, width, 3])
   label = tf.cast(parsed_features['label'], tf.int64)
   return image, label
@@ -32,10 +42,6 @@ d = d.shard(num_workers, worker_index)
 d = d.repeat(num_epochs)
 d = d.shuffle(shuffle_buffer_size)
 dataset = d.map(parser_fn, num_parallel_calls=num_map_threads)
-
-iterator = dataset.make_one_shot_iterator()
-images, labels = iterator.get_next()
-print(labels.numpy())
 
 for item, label in dataset:
   # print(item.numpy())
