1ac5e2fa2a47ec44541d0b6f4d480832cf3cbaab
le.zhang <le.zhang@silot.ai>
2020-02-25 00:14:59 +0800

    update runmodel

--- model/loadmdel.py
+++ model/loadmdel.py
@@ -12,11 +12,3 @@ base_root = '/Users/zhangle/Documents/IS/coffee2docker/'
 checkpoint_path = base_root + "/model/cp.ckpt"
 
 print(checkpoint_path)
-
-
-sess = tf.Session()
-with gfile.FastGFile(pb_file_path+'model.pb', 'rb') as f:
-    graph_def = tf.GraphDef()
-    graph_def.ParseFromString(f.read())
-    sess.graph.as_default()
-    tf.import_graph_def(graph_def, name='') # 导入计算图
--- model/runmodel.py
+++ model/runmodel.py
@@ -1,6 +1,8 @@
 import tensorflow as tf 
 from tensorflow import keras
 import sys
+sys.path.append('../')
+from dataset.generateBatch import dataset, testdataset
 
 base_root = '/Users/zhangle/Documents/IS/coffee2docker/model/'
 # base_root = '/Users/zhangle/Documents/TableDetect/coffee2docker/model/'
@@ -12,12 +14,27 @@ filename = 'IMG_0336.jpg'
 test_images = open('/Users/zhangle/Documents/IS/coffee2docker/dataset/testdata/' + filename, 'rb').read()
 test_labels = 0
 
+def create_model():
+  model = tf.keras.models.Sequential([
+    keras.layers.Conv2D(20, (5, 5), input_shape=(1512, 1209, 1), activation='relu'),
+    keras.layers.MaxPool2D(pool_size=(2, 2)),
+    keras.layers.Conv2D(40, (5, 5), activation='relu'),
+    keras.layers.MaxPool2D(pool_size=(2, 2)),
+    keras.layers.Dropout(0.2),
+    keras.layers.Flatten(),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(2, activation='softmax')
+  ])
+  model.compile(optimizer='adam',
+    loss=tf.losses.SparseCategoricalCrossentropy(from_logits=False),
+    metrics=['accuracy'])
+  return model
 
-model = tf.keras.models.load_model(checkpoint_path)
-model.summery()
+model = create_model()
+model.summary()
 
 print('load weight path')
 model.load_weights(checkpoint_path)
 
-loss,acc = model.evaluate(test_images,  test_labels, verbose=2)
+loss,acc = model.evaluate(testdataset, verbose=2)
 print("Restored model, accuracy: {:5.2f}%".format(100*acc))
