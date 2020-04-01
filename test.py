886be87f9385ab4e509916c028c09701e6e7f513
le.zhang <le.zhang@silot.ai>
2020-04-01 23:06:49 +0800

    update

--- test.py
+++ test.py
@@ -12,20 +12,18 @@ base_path = os.getcwd()
 
 filename = '/Users/zhangle/Documents/IS/T3CA1/annotations/instances_train2014.json'
 data = datasetUtil.read_json(filename)
-print(data['annotations'][0]['category_id'])
-print(data['images'][0]['width'])
-print(data['images'][0]['height'])
+# print(data['annotations'][0]['category_id'])
 
 images = []
 filenamelist = datasetUtil.read_images_list(base_path+'/data/annotation')
 
 data = datasetUtil.read_json(base_path + '/data/annotation/image_0.json')
-print(data['outputs']['object'][0]['name'])
+# print(data['size']['width'])
 
 annotations = []
 for item in filenamelist:
   data = datasetUtil.read_json(base_path + '/data/annotation/' + item)
-  polygon = data['outputs']['object'][0]['polygon'].values()
+  # polygon = data['outputs']['object'][0]['polygon']
   for i in data['outputs']['object']:
     id = 63
     if (i['name']) == 'sofa':
@@ -34,25 +32,28 @@ for item in filenamelist:
       id = 67
     if (i['name']) == 'chair':
       id = 62
+    polygon = []
+    for j in i['polygon'].values():
+      polygon.append(j)
     annotation = {
-      'segmentation': [i['polygon'].values()],
+      'segmentation': [polygon],
       'category_id': id,
       'image_id': item.replace('.json','.jpg')
     }
     annotations.append(annotation)
-  
+
   image = {
     'license': 1,
-    'file_name': item,
-    'height': 100,
-    'width': 200,
-    'id': 0 }
+    'file_name': item.replace('.json','.jpg'),
+    'height': data['size']['height'],
+    'width': data['size']['width'],
+    'id': item.replace('.json','.jpg') }
   images.append(image)
 
 
 
 cocofile = {
-  'info': {},
+  'info': { "version": "1.0" },
   'licenses': [],
   'images': images,
   'annotations': annotations,
@@ -64,4 +65,8 @@ cocofile = {
   ]
 }
 
-print(cocofile)
+
+filejson = base_path + '/data/coco.json'
+coco = json.dumps(cocofile)
+with open(filejson, 'w') as f:
+  f.write(coco)
